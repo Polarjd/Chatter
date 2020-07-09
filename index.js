514e6e24eb4370cc8b50bd5d107a8f9346e8d1a2
Polarbeardss <59700990+Polarbeardss@users.noreply.github.com>
2020-07-09 21:28:54 +0200

    Chatter

--- index.js
+++ index.js
@@ -116,6 +116,28 @@ client.on('message', message => {
 
         break;
 
+        case 'kick':
+            let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+            if(!kUser) message.channel.send("Can't find user");
+            let kReason = args.join(" ").slice(22);
+            if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.send("You do not have permission for that");
+            if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You cannot kick that user");
+
+            let kickEmbed = new Discord.RichEmbed()
+            .setDescription("~kick~")
+            .setColor("#e56b00")
+            .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+            .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+            .addField("Kicked In", message.channel)
+            .addField("Time", message.createdAt)
+            .addField("Reason", kReason);
+
+            let kickChannel = message.guild.channels.find(`name`, "incidents");
+            if(!kickChannel) return message.channel.send("Can't find incidents channel");
+
+            message.guild.member(kUser).kick(kReason);
+            kickChannel.send(kickEmbed);
+
 
 
 
