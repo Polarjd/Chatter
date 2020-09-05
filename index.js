bc4c4d5ad3e3acdeb9588daf24dc1a94652be8e5
Polarbeardss <59700990+Polarbeardss@users.noreply.github.com>
2020-09-05 15:28:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ client.on('message', async message => {
     if(cooldown.has(message.author.id)){
         return message.reply("Please wait 5 seconds before using this command again")
     }
-    if(!message.member.hasPermission("ADMINISTRATOR")){
-        cooldown.add(message.author.id);
-    }
+    //if(!message.member.hasPermission("ADMINISTRATOR")){
+    //    cooldown.add(message.author.id);
+    //}
 
     setTimeout(() => {
         cooldown.delete(message.author.id)
